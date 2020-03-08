e4b2e480a0a3f7e1f4353e5834e9a2871ac95abb
Andrew Aquino <andrew.aquino@tallan.com>
2020-03-08 10:09:56 -0700

    convert unique classes in footer to IDs

--- docs/.vuepress/theme/components/PageFooter.vue
+++ docs/.vuepress/theme/components/PageFooter.vue
@@ -2,11 +2,11 @@
   <footer id="page-footer" class="row no-gutters">
     <div class="col-12 col-md-4 px-4">
       <div class="logo"></div>
-      <div class="button-container w-100 my-5">
+      <div id="button-container" class="w-100 my-5">
         <a class="btn btn-lg btn-block btn-primary mb-3 py-3 font-weight-bold">CONTRIBUTE</a>
         <a class="btn btn-lg btn-block btn-secondary py-3 font-weight-bold">VOLUNTEER</a>
       </div>
-      <ul class="social-media-links list-inline">
+      <ul id="social-media-links" class="list-inline">
         <li class="list-inline-item">
           <a href="http://www.facebook.com/Cindyfordistrict7">
           <FaFacebook></FaFacebook>
@@ -25,14 +25,14 @@
       </ul>
     </div>
     <div class="col-12 col-md-8 px-5 text-center">
-      <ul class="footer-nav list-inline mb-0">
+      <ul id="footer-nav" class="list-inline mb-0">
         <li class="list-inline-item"><a href="/">Home</a></li>
         <li class="list-inline-item"><a href="/about/">Meet Cindy</a></li>
         <li class="list-inline-item"><a href="/priorities/">Priorities</a></li>
         <li class="list-inline-item"><a href="/contact/">Contact</a></li>
         <li class="list-inline-item"><a href="/district/">District</a></li>
       </ul>
-      <ul class="contact-info list-unstyled my-5">
+      <ul id="contact-info" class="list-unstyled my-5">
         <li>
           <a href="mailto:cindy@cindylerner.com">cindy@cindylerner.com</a>
         </li>
@@ -40,10 +40,10 @@
           <a href="tel:+13059923433">305.992.3433</a>
         </li>
       </ul>
-      <span class="disclaimer px-4 py-2">
+      <span id="disclaimer" class="px-4 py-2">
         POLITICAL ADVERTISEMENT PAID FOR AND APPROVED BY CINDY LERNER FOR COUNTY COMMISSION DISTRICT 7
       </span>
-      <a href="https://dawneraq.github.io" class="author-link mt-5">
+      <a href="https://dawneraq.github.io" id="author-link" class="mt-5">
         Website by Andrew Aquino
       </a>
     </div>
@@ -72,7 +72,6 @@ $leftContentWidth = 295px
 footer#page-footer
   background $accentColor
   color white
-
   > div
     display flex
     flex-direction column
@@ -89,7 +88,7 @@ footer#page-footer
       background-size contain
       background-repeat no-repeat
       background-position center
-    .button-container
+    #button-container
       max-width $leftContentWidth
       .btn
         border none
@@ -100,9 +99,9 @@ footer#page-footer
         background-color lighten($accentColor, 70%)
       .btn-secondary
         background-color $accentColor
-    .social-media-links > li:not(:last-child)
+    #social-media-links > li:not(:last-child)
       margin-right 13px
-    .footer-nav
+    #footer-nav
       li
         margin-left calc(31px/2)
         margin-right calc(31px/2)
@@ -110,15 +109,14 @@ footer#page-footer
           font-weight 900
           &:hover
             text-decoration none
-    .contact-info
+    #contact-info
       opacity 0.59
-    .disclaimer
+    #disclaimer
       opacity 0.73
       border 3px solid rgba(255, 255, 255, 0.23)
       max-width 350px
-    .author-link
+    #author-link
       opacity 0.3
-
   a:not(.btn)
     color white
     opacity 0.76

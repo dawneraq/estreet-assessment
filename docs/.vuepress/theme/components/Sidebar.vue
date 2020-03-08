da87a577b6f49b920a601cd033209021f6182762
Andrew Aquino <andrew.aquino@tallan.com>
2020-03-08 14:45:06 -0700

    style sidebar's #donate-button

--- docs/.vuepress/theme/components/Sidebar.vue
+++ docs/.vuepress/theme/components/Sidebar.vue
@@ -37,13 +37,16 @@ export default {
     display none
     border-bottom 1px solid $borderColor
     padding 0.5rem 0 0.75rem 0
-    a
+    a:not(#donate-button)
       font-weight 600
     .nav-item, .repo-link
       display block
       line-height 1.25rem
       font-size 1.1em
       padding 0.5rem 0 0.5rem 1.5rem
+    #donate-button
+      margin-left 20px
+      margin-right 20px
   & > .sidebar-links
     padding 1.5rem 0
     & > li > a.sidebar-link

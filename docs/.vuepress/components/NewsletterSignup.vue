943f82a76dee2d92e5e12c15bf7bde8d19c016fd
Andrew Aquino <andrew.aquino@tallan.com>
2020-03-08 20:24:34 -0700

    add styles to position/size #signup-content

--- docs/.vuepress/components/NewsletterSignup.vue
+++ docs/.vuepress/components/NewsletterSignup.vue
@@ -3,15 +3,21 @@
     class="newsletter-signup"
     :style="style"
   >
-    <p class="lead text-light">{{ secondaryText }}</p>
-    <h1 class="text-white">{{ heading }}</h1>
-    <form>
-      <input type="text" placeholder="Name">
-      <input type="text" placeholder="Email">
-      <button class="btn btn-lg btn-light">
-        GET INVOLVED
-      </button>
-    </form>
+    <div class="container h-100 d-flex align-items-center">
+      <div id="signup-content" class="text-center">
+        <p class="lead text-light">{{ secondaryText }}</p>
+        <h1 class="text-white mb-3">{{ heading }}</h1>
+        <form class="container-fluid p-0">
+          <div class="row no-gutters">
+            <input type="text" placeholder="Name" required>
+            <input type="text" placeholder="Email" required>
+          </div>
+          <button class="btn btn-lg btn-block btn-light">
+            GET INVOLVED
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,4 +47,13 @@ export default {
   &:last-of-type
     height 565px
     margin-bottom -2rem
+  #signup-content
+    width 370px
+    input
+      width 50%
+      &:not(:last-child)
+        border-right 1px solid rgb(210, 210, 210)
+    button
+      height 60px
+      margin-top 10px
 </style>
